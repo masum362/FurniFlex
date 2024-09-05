e4820ad834e16b30cab48dae36b3950b099db0cb
masum362 <masumahmed64077@gmail.com>
2024-09-05 19:59:55 +0600

    added different pages

--- client/src/router/router.jsx
+++ client/src/router/router.jsx
@@ -1,22 +1,26 @@
+import App from '@/App';
+import Home from '@/pages/home/Home';
+import Login from '@/pages/login/Login';
+import SignUp from '@/pages/signUp/SignUp';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
     {
-        path:"",
-        element:<div>Home</div>,
-        errorElement:<div>Error...</div>,
-        children:[
+        path: "",
+        element: <App />,
+        errorElement: <div>Error...</div>,
+        children: [
             {
-                path:"/",
-                element:<div>Home</div>,
+                path: "/",
+                element: <Home />,
             },
             {
-                path:"/login",
-                element:<div>Login</div>,
+                path: "/login",
+                element: <Login />,
             },
             {
-                path:"/signup",
-                element:<div>Sign Up</div>,
+                path: "/signup",
+                element: <SignUp />,
             }
         ]
     }
